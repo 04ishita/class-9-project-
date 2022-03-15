7b1fd4b7dd7228fcb64d96f244cef5531cb8a0b0
04ishita <89136493+04ishita@users.noreply.github.com>
2022-03-15 03:39:55 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var object 
+function setup() {
+  createCanvas(400,400);
+  
+}
+
+function draw() 
+{
+  
+
+  if(keyIsDown(DOWN_ARROW)){
+
+    background("red")
+  }
+
+if (keyIsDown(LEFT_ARROW)){
+  background ("yellow")
+}
+
+if (keyIsDown(RIGHT_ARROW)){
+  background("blue")
+}
+
+if(keyIsDown(UP_ARROW)){
+  background("pink")
+}
+}
+
+
+
+
